f48f9c20a95a1300ed1f0f2ff917d9049585d910
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2019-09-27 07:10:24 +0700

    drone: lavender: R6 android 10 branch

--- drone-lavender-clang.sh
+++ drone-lavender-clang.sh
@@ -22,7 +22,7 @@ mkdir Clarity-TEMP
 apt-get install -y ccache bc git-core gnupg build-essential zip curl make automake autogen autoconf autotools-dev libtool shtool python m4 gcc libtool zlib1g-dev dash libssl-dev
 
 # Cloning Kernel Repository
-git clone https://github.com/Nicklas373/kernel_xiaomi_lavender -b dev/staging/yukina lavender
+git clone https://github.com/Nicklas373/kernel_xiaomi_lavender -b dev/10/yukina lavender
 
 # Workaround For Directory
 cd lavender
